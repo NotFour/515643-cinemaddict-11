fad634b3f19259f0e7ddac0c7211173144d49ef3
NotFour <NotFour@yandex.ru>
2020-05-13 12:04:25 +0300

    module3-task1 add structures of date, refactor functions of get components

--- src/components/filmPopup.js
+++ src/components/filmPopup.js
@@ -1,4 +1,16 @@
-const getFilmPopup = function () {
+import {getComment} from './comment.js';
+
+const getFilmPopup = function (film) {
+  let commentElements = ``;
+  let genreElements = ``;
+  for (const comment of film.comments) {
+    commentElements += getComment(comment);
+  }
+
+  for (const genre of film.filmInfo.genre) {
+    genreElements += `<span class="film-details__genre">${genre}</span>`;
+  }
+
   return `
     <section class="film-details">
       <form class="film-details__inner" action="" method="get">
@@ -8,59 +20,57 @@ const getFilmPopup = function () {
           </div>
           <div class="film-details__info-wrap">
             <div class="film-details__poster">
-              <img class="film-details__poster-img" src="./images/posters/the-great-flamarion.jpg" alt="">
+              <img class="film-details__poster-img" src="${film.filmInfo.poster}" alt="">
 
-              <p class="film-details__age">18+</p>
+              <p class="film-details__age">${film.filmInfo.ageRating}</p>
             </div>
 
             <div class="film-details__info">
               <div class="film-details__info-head">
                 <div class="film-details__title-wrap">
-                  <h3 class="film-details__title">The Great Flamarion</h3>
-                  <p class="film-details__title-original">Original: The Great Flamarion</p>
+                  <h3 class="film-details__title">${film.filmInfo.title}</h3>
+                  <p class="film-details__title-original">${film.filmInfo.alternativeTitle}</p>
                 </div>
 
                 <div class="film-details__rating">
-                  <p class="film-details__total-rating">8.9</p>
+                  <p class="film-details__total-rating">${film.filmInfo.totalRating}</p>
                 </div>
               </div>
 
               <table class="film-details__table">
                 <tr class="film-details__row">
                   <td class="film-details__term">Director</td>
-                  <td class="film-details__cell">Anthony Mann</td>
+                  <td class="film-details__cell">${film.filmInfo.director}</td>
                 </tr>
                 <tr class="film-details__row">
                   <td class="film-details__term">Writers</td>
-                  <td class="film-details__cell">Anne Wigton, Heinz Herald, Richard Weil</td>
+                  <td class="film-details__cell">${film.filmInfo.writers}</td>
                 </tr>
                 <tr class="film-details__row">
                   <td class="film-details__term">Actors</td>
-                  <td class="film-details__cell">Erich von Stroheim, Mary Beth Hughes, Dan Duryea</td>
+                  <td class="film-details__cell">${film.filmInfo.actors}</td>
                 </tr>
                 <tr class="film-details__row">
                   <td class="film-details__term">Release Date</td>
-                  <td class="film-details__cell">30 March 1945</td>
+                  <td class="film-details__cell">${film.filmInfo.release.date}</td>
                 </tr>
                 <tr class="film-details__row">
                   <td class="film-details__term">Runtime</td>
-                  <td class="film-details__cell">1h 18m</td>
+                  <td class="film-details__cell">${film.filmInfo.runtime}</td>
                 </tr>
                 <tr class="film-details__row">
                   <td class="film-details__term">Country</td>
-                  <td class="film-details__cell">USA</td>
+                  <td class="film-details__cell">${film.filmInfo.release.releaseCountry}</td>
                 </tr>
                 <tr class="film-details__row">
                   <td class="film-details__term">Genres</td>
                   <td class="film-details__cell">
-                    <span class="film-details__genre">Drama</span>
-                    <span class="film-details__genre">Film-Noir</span>
-                    <span class="film-details__genre">Mystery</span></td>
+                    ${genreElements}
                 </tr>
               </table>
 
               <p class="film-details__film-description">
-                The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Great Flamarion (Erich von Stroheim) is an arrogant, friendless, and misogynous marksman who displays his trick gunshot act in the vaudeville circuit. His show features a beautiful assistant, Connie (Mary Beth Hughes) and her drunken husband Al (Dan Duryea), Flamarion's other assistant. Flamarion falls in love with Connie, the movie's femme fatale, and is soon manipulated by her into killing her no good husband during one of their acts.
+                ${film.filmInfo.description}
               </p>
             </div>
           </div>
@@ -79,9 +89,9 @@ const getFilmPopup = function () {
 
         <div class="form-details__bottom-container">
           <section class="film-details__comments-wrap">
-            <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">0</span></h3>
+            <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${film.comments.length}</span></h3>
 
-            <ul class="film-details__comments-list"></ul>
+            <ul class="film-details__comments-list">${commentElements}</ul>
 
             <div class="film-details__new-comment">
               <div for="add-emoji" class="film-details__add-emoji-label">
--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import {getSortMenu} from './components/sortMenu.js';
 import {getFilmContainer} from './components/filmContainer.js';
 import {getFilmCard} from './components/filmCard.js';
 import {getShowMore} from './components/showMore.js';
-import {getFilmContainerExtra} from './components/filmContainerExtra.js';
 import {getFilmsStatistic} from './components/filmStat.js';
 import {getFilmPopup} from './components/filmPopup.js';
 
@@ -12,37 +11,186 @@ const renderComponent = function (template, element) {
   template.innerHTML += element;
 };
 
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const getRandomElems = function (mass, count = 1) {
+  const iterator = (typeof count === `number`) ? count : getRandomNumber(...count);
+
+  const result = [];
+  let randomElem;
+  for (let i = 0; i < iterator; i++) {
+    randomElem = mass[getRandomNumber(0, mass.length - 1)];
+    result.push(randomElem);
+  }
+
+  return result;
+};
+
+const createFilmObject = function () {
+  const posters = [`made-for-each-other.png`, `popeye-meets-sinbad.png`, `sagebrush-trail.jpg`,
+    `santa-claus-conquers-the-martians.jpg`, `the-dance-of-life.jpg`, `the-great-flamarion.jpg`, `the-man-with-the-golden-arm.jpg`];
+  const labels = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus`.split(/\.{1} /).map((sentense) => {
+    return `${sentense}. `;
+  });
+  const emotions = [`smile`, `sleeping`, `puke`, `angry`];
+  const filmDuration = getRandomNumber(60, 180);
+
+  const comments = [
+    {
+      id: `42`,
+      author: `Ilya O'Reilly`,
+      comment: `a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.`,
+      date: `2019/12/31 23:59`,
+      emotion: getRandomElems(emotions)
+    },
+    {
+      id: `42`,
+      author: `Ilya O'Reilly`,
+      comment: `a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.`,
+      date: `2019/12/31 23:59`,
+      emotion: getRandomElems(emotions)
+    },
+    {
+      id: `42`,
+      author: `Ilya O'Reilly`,
+      comment: `a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.`,
+      date: `2019/12/31 23:59`,
+      emotion: getRandomElems(emotions)
+    },
+    {
+      id: `42`,
+      author: `Ilya O'Reilly`,
+      comment: `a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.`,
+      date: `2019/12/31 23:59`,
+      emotion: getRandomElems(emotions)
+    },
+    {
+      id: `42`,
+      author: `Ilya O'Reilly`,
+      comment: `a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.`,
+      date: `2019/12/31 23:59`,
+      emotion: getRandomElems(emotions)
+    },
+  ];
+
+  return {
+    id: `0`,
+    comments: getRandomElems(comments, [0, 5]),
+    filmInfo: {
+      title: `A Little Pony Without The Carpet`,
+      alternativeTitle: `Original: ${`Laziness Who Sold Themselves`}`,
+      totalRating: 5.3,
+      poster: `./images/posters/${getRandomElems(posters)}`,
+      ageRating: `${0}+`,
+      director: `Tom Ford`,
+      writers: [
+        `Takeshi Kitano`,
+        `Takeshi Kitano`,
+        `Takeshi Kitano`
+      ].join(`, `),
+      actors: [
+        `Morgan Freeman`,
+        `Morgan Freeman`
+      ].join(`, `),
+      release: {
+        date: `30 March 1945`,
+        year: `2019`,
+        releaseCountry: `Finland`
+      },
+      runtime: `${Math.floor(filmDuration / 60)}h ${filmDuration % 60}m`,
+      genre: [
+        `Comedy`,
+        `thriller`,
+        `melodramma`
+      ],
+      description: getRandomElems(labels, [1, 5]).join(``),
+    },
+    userDetails: {
+      watchlist: getRandomNumber(0, 1),
+      alreadyWatched: getRandomNumber(0, 1),
+      watchingDate: `2019-04-12T16:12:32.554Z`,
+      favorite: getRandomNumber(0, 1)
+    }
+  };
+};
+
+const createMocks = function (count) {
+  const result = [];
+
+  for (let i = 0; i < count; i++) {
+    result.push(createFilmObject());
+  }
+
+  return result;
+};
+
+const mocks = createMocks(20);
+
+const createFiltersObject = function () {
+  let watchlist = 0; let history = 0; let favorites = 0;
+
+  for (const mock of mocks) {
+    watchlist += mock.userDetails.watchlist ? mock.userDetails.watchlist : 0;
+    history += mock.userDetails.alreadyWatched ? mock.userDetails.alreadyWatched : 0;
+    favorites += mock.userDetails.favorite ? mock.userDetails.favorite : 0;
+  }
+
+  return {
+    watchlist,
+    history,
+    favorites,
+  };
+};
+
+const filters = createFiltersObject();
+
 const header = document.querySelector(`.header`);
 renderComponent(header, getUserLevel());
 
 const mainContainer = document.querySelector(`.main`);
 
-renderComponent(mainContainer, getMainMenu());
+renderComponent(mainContainer, getMainMenu(filters));
 renderComponent(mainContainer, getSortMenu());
 renderComponent(mainContainer, getFilmContainer());
 
-const filmsSection = document.querySelector(`.films`);
 const filmsList = document.querySelector(`.films .films-list`);
-const filmsListContainer = document.querySelector(`.films-list__container`);
+let filmsListContainer = document.querySelector(`.films .films-list .films-list__container`);
 
-for (let i = 0; i < 5; i++) {
-  renderComponent(filmsListContainer, getFilmCard());
+let filmsShowed = 0;
+
+for (filmsShowed; filmsShowed < 5; filmsShowed++) {
+  renderComponent(filmsListContainer, getFilmCard(mocks[filmsShowed]));
 }
 
 renderComponent(filmsList, getShowMore());
-renderComponent(filmsSection, getFilmContainerExtra(`Top rated`));
-renderComponent(filmsSection, getFilmContainerExtra(`Most commented`));
-
-const filmsExtraListContainers = document.querySelectorAll(`.films-list--extra .films-list__container`);
-
-filmsExtraListContainers.forEach(function (filmContainer) {
-  for (let i = 0; i < 2; i++) {
-    renderComponent(filmContainer, getFilmCard());
-  }
-});
 
 const footerStatistic = document.querySelector(`.footer__statistics`);
 
-renderComponent(footerStatistic, getFilmsStatistic());
-renderComponent(document.body, getFilmPopup());
+renderComponent(footerStatistic, getFilmsStatistic(`130 291`));
+renderComponent(document.body, getFilmPopup(mocks[0]));
 
+
+let filmsShowedLater = filmsShowed;
+
+filmsListContainer = document.querySelector(`.films .films-list .films-list__container`);
+const showMoreBtn = document.querySelector(`.films-list__show-more`);
+
+const showMore = function () {
+
+  for (filmsShowed; filmsShowed < filmsShowedLater + 5 && filmsShowed < mocks.length; filmsShowed++) {
+    renderComponent(filmsListContainer, getFilmCard(mocks[filmsShowed]));
+  }
+
+  filmsShowedLater = filmsShowed;
+
+  if (filmsShowed === mocks.length) {
+    showMoreBtn.removeEventListener(`click`, showMore);
+    showMoreBtn.classList.add(`visually-hidden`);
+  }
+};
+
+showMoreBtn.addEventListener(`click`, showMore);
